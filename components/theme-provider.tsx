17ff7f4a6578dafb9822067bfb12c6af29feb937
baba786 <sudhanshu9252@gmail.com>
2025-01-19 00:25:07 +0530

    Refactor ThemeProviderProps interface in theme-provider.tsx to explicitly define props.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,16 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+  forcedTheme?: string;
+  storageKey?: string;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
